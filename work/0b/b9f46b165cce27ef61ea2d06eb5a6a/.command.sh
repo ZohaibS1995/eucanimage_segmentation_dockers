ce7b07a740da68e33b53cb0e19a2ed5c6a731144
bscuser <ana.redondo@bsc.es>
2025-08-14 14:22:23 +0200

    clean setup of the workflow

--- work/0b/b9f46b165cce27ef61ea2d06eb5a6a/.command.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash -ue
-python3 /app/compute_metrics.py -i brain.nii.tar.gz -c ECI_UC0_Seg -e Brain_Cancer_Diagnosis -g goldstandard_dir -p tool1 -com EuCanImage -o "assessment_results.json"
--- work/f5/bfe3fc9f78ed2b7f4dc1368c9c71c2/.command.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash -ue
-python3 /app/validation.py -i segmentations.nii.tar.gz -com EuCanImage -c ECI_UC0_Seg -e Brain_Cancer_Diagnosis -p tool1 -g goldstandard_dir
